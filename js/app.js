996b075df6d8bd7409e47c69344ba18987aaf8c0
vishwanathsinghyadav <singhvishwanathyadav@gmail.com>
2023-05-19 23:47:58 +0530

    made oeuvre page

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const filter_btns = document.querySelectorAll(".filter-btn");
+
+filter_btns.forEach((btn)=> btn.addEventListener("click",()=>{
+    filter_btns.forEach((button)=>button.classList.remove("active"));
+    btn.classList.add("active")
+
+    let filterValue = btn.dataset.filter;
+
+    $(".grid").isotope({filter: filterValue});
+}));
+
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    transitonDuration: "0.6s",
+  });
